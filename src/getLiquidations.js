071a15096d8ff384714836e99988dd403e94bbd2
mpereiraesaa <manuelpereiralds@gmail.com>
2021-11-20 23:19:56 -0300

    Increase minimum profit

--- src/getLiquidations.js
+++ src/getLiquidations.js
@@ -25,7 +25,7 @@ let LIQUIDATION_IN_PROCESS = false;
 
 const liquidator = new ethers.Contract(currentConfiguration.liquidator, LiquidatorABI, provider);
 
-const MINIMUM_PROFITABLE_DEBT = 35; // USD
+const MINIMUM_PROFITABLE_DEBT = 100; // USD
 
 async function run() {
   const { timestamp } = await provider.getBlock('latest');
